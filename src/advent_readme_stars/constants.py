a4455d52285d47a106f99b93e7661e5b86aaf508
Thomas Feuerbach <thfeuerbach@gmail.com>
2022-12-01 22:04:51 -0500

    Update constants.py

--- src/advent_readme_stars/constants.py
+++ src/advent_readme_stars/constants.py
@@ -27,7 +27,8 @@ HEADER_PREFIX = os.environ.get("INPUT_HEADERPREFIX", "")
 README_LOCATION = os.environ.get("INPUT_READMELOCATION", "")
 
 #: Advent of Code base URL, for testing
-ADVENT_URL = os.environ.get("ADVENT_URL", "https://adventofcode.com")
+# ADVENT_URL = os.environ.get("ADVENT_URL", "https://adventofcode.com")
+ADVENT_URL = os.environ.get("ADVENT_URL", "https://github.com/tfeuerbach/advent_of_code/tree/main/")
 
 #: Stars info endpoint
 STARS_ENDPOINT = f"{ADVENT_URL}/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}.json"
